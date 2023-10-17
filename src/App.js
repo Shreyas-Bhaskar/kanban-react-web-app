0998b99bb52133c2c9c9a9de523899e619df813e
shreyas bhaskar <bhaskar.s@northeastern.edu>
2023-10-17 13:27:32 -0400

    A3 init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Labs from "./Labs";
+import HelloWorld from "./Labs/a3/HelloWorld";
+import Kanbas from "./Kanbas";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Shreyas Bhaskar : CS5610 Fall 23 - Assignments
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <div>
+         <HelloWorld/>
+         <Labs/>
+         <Kanbas/>
+      </div>
+   );
 }
-
 export default App;
---
+++ src/Labs/a3/JavaScript.js
@@ -0,0 +1,14 @@
+
+import VariablesAndConstants from "./VariablesandConstants";
+import VariableTypes from "./VariableTypes";
+function JavaScript() {
+    return(
+       <div>
+          <h1>JavaScript</h1>
+          <VariablesAndConstants/>
+          <VariableTypes/>
+       </div>
+       
+    );
+ }
+ export default JavaScript
