535b783ea19dc2b1e057f33a88f02d4b987e26e1
shreyas bhaskar <bhaskar.s@northeastern.edu>
2023-10-17 20:09:48 -0400

    Added assignments and stuff

--- src/Labs/a3/index.js
+++ src/Labs/a3/index.js
@@ -2,6 +2,10 @@ import JavaScript from "./JavaScript";
 import Nav from "../../Nav";
 import Classes from "./Classses";
 import ConditionalOutput from "./ConditionalOutput";
+import TodoItem from "./todos/TodoItem";
+import TodoList from "./todos/TodoList";
+import Styles from "./Styles";
+import PathParameters from "./PathParameters";
 function Assignment3() {
     console.log('Hello World!');
     return(
@@ -10,6 +14,9 @@ function Assignment3() {
           <ConditionalOutput/>
           <Classes/>
           <JavaScript/>
+          <TodoItem/>
+            <TodoList/>
+            <Styles/>
        </div>
     );
  }
