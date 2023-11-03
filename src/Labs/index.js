78792557907a0627f201a7e4f8a683a6a3b47b55
shreyas bhaskar <bhaskar.s@northeastern.edu>
2023-11-02 20:08:39 -0400

    edits

--- src/Labs/index.js
+++ src/Labs/index.js
@@ -37,7 +37,6 @@ function Labs() {
           <Route path="/" element={<Navigate to="a3" />} />
           <Route path="a3/*" element={<Assignment3 />} />
           <Route path="a4" element={<Assignment4 />} />
-          <Route path="a5" element={<Assignment5 />} />
         </Routes>
         {/* <Assignment3 />
       <Assignment4 />
