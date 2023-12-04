e44e90a660477a78c645772c7e215fd61951308f
shreyas bhaskar <bhaskar.s@northeastern.edu>
2023-12-04 15:08:33 -0500

    A6

---
+++ src/Project/home.js
@@ -0,0 +1,11 @@
+function Home() {
+    return (
+      <div>
+        <h2>Home</h2>
+        <p>
+          This is the User home. All Db operations related to User is done here
+        </p>
+      </div>
+    );
+  }
+  export default Home;
